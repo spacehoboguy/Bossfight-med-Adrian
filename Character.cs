13763ed4ec240438de5edb772cce8a178678a3eb
Philip Olafsen <ph.olafsen@gmail.com>
2023-06-06 14:26:47 +0200

    intial commit of bossfight

---
+++ Character.cs
@@ -0,0 +1,54 @@
+﻿namespace Bossfight_med_Adrian;
+
+public class GameCharacter
+{
+    public string? Name { get; set; }
+
+    public int Health { get; set; } 
+
+    public int Strength { get; set; }
+
+    public int Stamina { get; set; }
+
+    //IMAN NOTES: Trekke ut changeHealth(this) fra fight i en egen methode. 
+    //Bruke .this 
+
+    public GameCharacter(string? name, int health, int strength, int stamina)
+    {
+        Name = name;
+        Health = health;
+        Strength = strength;
+        Stamina = stamina;
+    }
+
+    public void Fight(GameCharacter fiende)
+    {
+        if (Stamina > 0)
+        {
+            Stamina -= 10;
+            var fiendeHealth = fiende.Health - Strength;
+            fiende.Health = fiendeHealth;
+            Console.WriteLine($"{Name} attacked {fiende.Name} for {Strength}HP, {fiende.Name} has {fiende.Health}HP' remaining.");
+            
+        }
+        else
+        {
+            //Recharge(); 
+            Console.WriteLine("Stamina must be recharged to fight on!");
+        }
+    }
+
+    public void Recharge(GameCharacter charToCharge)
+    {
+        if (charToCharge.Name == "EvilBoss")
+        {
+            Stamina += 10;
+            Console.WriteLine($"{Name} recharged stamina to {Stamina}");
+        }
+        else
+        {
+            Stamina += 40;
+            Console.WriteLine($"{Name} recharged stamina to {Stamina}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Enter showdown");
+﻿using Bossfight_med_Adrian;
+
+InitializeFight();
+
+void InitializeFight()
+{
+    Console.WriteLine("Enter showdown");
+    Console.WriteLine("Character Name:");
+    string? inputName = Console.ReadLine();
+    GameCharacter boss = new GameCharacter("EvilBoss", 400, 30, 10);
+    GameCharacter hero = new GameCharacter(inputName, 100, 20, 40);
+    
+
+    hero.Fight(boss);
+    boss.Fight(hero);
+    hero.Fight(boss);
+    boss.Recharge(boss);
+}
